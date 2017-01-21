7bd5313188e82fc1fab015868e0bc690a618f6b1
Valentin DESPORTES <vdesportes@gmail.com>
2017-01-21 11:20:35 +0100

    gen(Product)

---
+++ src/main/java/fr/despoval/notifei/domain/Product.java
@@ -0,0 +1,75 @@
+package fr.despoval.notifei.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Product.
+ */
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        if (product.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+            "id=" + id +
+            ", name='" + name + "'" +
+            '}';
+    }
+}
---
+++ src/test/java/fr/despoval/notifei/web/rest/ProductResourceIntTest.java
@@ -0,0 +1,237 @@
+package fr.despoval.notifei.web.rest;
+
+import fr.despoval.notifei.NotifApp;
+
+import fr.despoval.notifei.domain.Product;
+import fr.despoval.notifei.repository.ProductRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the ProductResource REST controller.
+ *
+ * @see ProductResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = NotifApp.class)
+public class ProductResourceIntTest {
+
+    private static final String DEFAULT_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_NAME = "BBBBBBBBBB";
+
+    @Inject
+    private ProductRepository productRepository;
+
+    @Inject
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Inject
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Inject
+    private EntityManager em;
+
+    private MockMvc restProductMockMvc;
+
+    private Product product;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        ProductResource productResource = new ProductResource();
+        ReflectionTestUtils.setField(productResource, "productRepository", productRepository);
+        this.restProductMockMvc = MockMvcBuilders.standaloneSetup(productResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Product createEntity(EntityManager em) {
+        Product product = new Product()
+                .name(DEFAULT_NAME);
+        return product;
+    }
+
+    @Before
+    public void initTest() {
+        product = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createProduct() throws Exception {
+        int databaseSizeBeforeCreate = productRepository.findAll().size();
+
+        // Create the Product
+
+        restProductMockMvc.perform(post("/api/products")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(product)))
+            .andExpect(status().isCreated());
+
+        // Validate the Product in the database
+        List<Product> productList = productRepository.findAll();
+        assertThat(productList).hasSize(databaseSizeBeforeCreate + 1);
+        Product testProduct = productList.get(productList.size() - 1);
+        assertThat(testProduct.getName()).isEqualTo(DEFAULT_NAME);
+    }
+
+    @Test
+    @Transactional
+    public void createProductWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = productRepository.findAll().size();
+
+        // Create the Product with an existing ID
+        Product existingProduct = new Product();
+        existingProduct.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restProductMockMvc.perform(post("/api/products")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(existingProduct)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Alice in the database
+        List<Product> productList = productRepository.findAll();
+        assertThat(productList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void checkNameIsRequired() throws Exception {
+        int databaseSizeBeforeTest = productRepository.findAll().size();
+        // set the field null
+        product.setName(null);
+
+        // Create the Product, which fails.
+
+        restProductMockMvc.perform(post("/api/products")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(product)))
+            .andExpect(status().isBadRequest());
+
+        List<Product> productList = productRepository.findAll();
+        assertThat(productList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void getAllProducts() throws Exception {
+        // Initialize the database
+        productRepository.saveAndFlush(product);
+
+        // Get all the productList
+        restProductMockMvc.perform(get("/api/products?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(product.getId().intValue())))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())));
+    }
+
+    @Test
+    @Transactional
+    public void getProduct() throws Exception {
+        // Initialize the database
+        productRepository.saveAndFlush(product);
+
+        // Get the product
+        restProductMockMvc.perform(get("/api/products/{id}", product.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(product.getId().intValue()))
+            .andExpect(jsonPath("$.name").value(DEFAULT_NAME.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingProduct() throws Exception {
+        // Get the product
+        restProductMockMvc.perform(get("/api/products/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateProduct() throws Exception {
+        // Initialize the database
+        productRepository.saveAndFlush(product);
+        int databaseSizeBeforeUpdate = productRepository.findAll().size();
+
+        // Update the product
+        Product updatedProduct = productRepository.findOne(product.getId());
+        updatedProduct
+                .name(UPDATED_NAME);
+
+        restProductMockMvc.perform(put("/api/products")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedProduct)))
+            .andExpect(status().isOk());
+
+        // Validate the Product in the database
+        List<Product> productList = productRepository.findAll();
+        assertThat(productList).hasSize(databaseSizeBeforeUpdate);
+        Product testProduct = productList.get(productList.size() - 1);
+        assertThat(testProduct.getName()).isEqualTo(UPDATED_NAME);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingProduct() throws Exception {
+        int databaseSizeBeforeUpdate = productRepository.findAll().size();
+
+        // Create the Product
+
+        // If the entity doesn't have an ID, it will be created instead of just being updated
+        restProductMockMvc.perform(put("/api/products")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(product)))
+            .andExpect(status().isCreated());
+
+        // Validate the Product in the database
+        List<Product> productList = productRepository.findAll();
+        assertThat(productList).hasSize(databaseSizeBeforeUpdate + 1);
+    }
+
+    @Test
+    @Transactional
+    public void deleteProduct() throws Exception {
+        // Initialize the database
+        productRepository.saveAndFlush(product);
+        int databaseSizeBeforeDelete = productRepository.findAll().size();
+
+        // Get the product
+        restProductMockMvc.perform(delete("/api/products/{id}", product.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate the database is empty
+        List<Product> productList = productRepository.findAll();
+        assertThat(productList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+}
